61c4cf8e1ae19d75dfebfbbb432b8360ef4c9cb3
bluesb2024 <end.syuryo@gmail.com>
2024-12-18 04:02:38 +0900

    WYV

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Universität Altridaz-Wyvern Motion-0.12";
+const cacheName = "Universität Altridaz-Wyvern Motion-0.21";
 const contentToCache = [
     "Build/Wyvern_motion_web.loader.js",
     "Build/Wyvern_motion_web.framework.js",
